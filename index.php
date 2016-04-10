6f7a0036da1f87e090f12035c09c26d9618601e3
edchtzwe <7440820@gmail.com>
2016-04-10 22:18:04 +1000

    Hello world

---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD><TITLE>Enter Page Title Here</TITLE>
+<META content="text/html; charset=utf-8" http-equiv=Content-Type>
+
+<STYLE type=text/css>BODY {
+	FONT-SIZE: 12px; FONT-FAMILY: verdana, arial, sans-serif
+}
+</STYLE>
+</HEAD>
+<BODY>Hello world!</BODY></HTML>
